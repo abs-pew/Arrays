1568b7e2c55dbf815f87e22a8c33441e66694116
abdulsalam <abs.pew@gmail.com>
2025-06-26 10:20:18 +0200

    Erste Aufgabe Arrays - Main Class

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // Aufgabe 01 - String Array
+// String[] studentNames = {"Florian", "Martin", "Domijnic", "Sara"};
+        String[] studentNames = new String[4];
+        studentNames[0] = "Florian";
+        studentNames[1] = "Martin";
+        studentNames[2] = "Dominic";
+        studentNames[3] = "Sara";
+
+
+    }
+}
